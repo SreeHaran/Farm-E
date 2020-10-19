f4bc3e23162c6ad14638ed1145a76f8a57ef5451
Sree Haran <23sreeharan@gmail.com>
2020-10-19 19:58:40 +0530

    Added Firebase Login UI

--- app/src/main/java/com/sreeharan/farm_e/LandDetailsActivity.java
+++ app/src/main/java/com/sreeharan/farm_e/LandDetailsActivity.java
@@ -1,14 +1,60 @@
 package com.sreeharan.farm_e;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
 
 public class LandDetailsActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1;
+    private FirebaseAuth mFirebaseAuth;
+    private FirebaseAuth.AuthStateListener mAuthStateListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_land_details);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        Button contact = findViewById(R.id.contact_button);
+        contact.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mFirebaseAuth.addAuthStateListener(mAuthStateListener);
+            }
+        });
+
+        mAuthStateListener = new FirebaseAuth.AuthStateListener(){
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    //signed in
+                    Toast.makeText(LandDetailsActivity.this, "Contacting the owner..", Toast.LENGTH_SHORT).show();
+                }else{
+                    //signed out
+                    Toast.makeText(LandDetailsActivity.this, "You need to sign in..", Toast.LENGTH_SHORT).show();
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setIsSmartLockEnabled(false)
+                                    .setAvailableProviders(Arrays.asList(
+                                        new AuthUI.IdpConfig.GoogleBuilder().build(),
+                                        new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                }
+            }
+        };
     }
 }
--- app/src/main/java/com/sreeharan/farm_e/MainActivity.java
+++ app/src/main/java/com/sreeharan/farm_e/MainActivity.java
@@ -2,7 +2,12 @@ package com.sreeharan.farm_e;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -15,6 +20,7 @@ public class MainActivity extends AppCompatActivity implements LandAdapter.LandI
     private LandAdapter mAdapter;
     private RecyclerView mLandList;
 
+    private static boolean isLoggedIn = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,9 +37,44 @@ public class MainActivity extends AppCompatActivity implements LandAdapter.LandI
     }
 
     @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        menu.removeItem(isLoggedIn?R.id.sign_out:R.id.sign_up);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_auth_state, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_up:
+                isLoggedIn = (!isLoggedIn);
+                Toast.makeText(this, "Signed in", Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.sign_out:
+                isLoggedIn = (!isLoggedIn);
+                Toast.makeText(this, "Signed out", Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            case R.id.toggle_button:
+                isLoggedIn = (!isLoggedIn);
+                Toast.makeText(this, "logged in is set to "+isLoggedIn, Toast.LENGTH_SHORT).show();
+                invalidateOptionsMenu();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void onLandItemClick(int clickedLandIndex) {
         Intent i = new Intent(MainActivity.this, LandDetailsActivity.class);
         startActivity(i);
-
     }
 }
